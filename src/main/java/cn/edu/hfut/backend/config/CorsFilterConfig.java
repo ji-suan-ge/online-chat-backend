a447aa759349c7358433ece4a505644f3b962256
xuewenG <gexuewen@outlook.com>
2020-06-27 16:18:59 +0800

    fix(config): add-cors-config

---
+++ src/main/java/cn/edu/hfut/backend/config/CorsFilterConfig.java
@@ -0,0 +1,39 @@
+package cn.edu.hfut.backend.config;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class CorsFilterConfig {
+    @Bean
+    public FilterRegistrationBean<CorsFilter> registrationCorsFilterBean() {
+        FilterRegistrationBean<CorsFilter> registrationBean = new FilterRegistrationBean<>();
+        registrationBean.setFilter(new CorsFilter());
+        registrationBean.addUrlPatterns("/*");
+        registrationBean.setName("corsFilter");
+        return registrationBean;
+    }
+
+    class CorsFilter implements Filter {
+        @Override
+        public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain)
+                throws IOException, ServletException {
+            HttpServletRequest request = (HttpServletRequest) servletRequest;
+            HttpServletResponse response = (HttpServletResponse) servletResponse;
+            response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
+            response.setHeader("Access-Control-Allow-Methods", "*");
+            response.setHeader("Access-Control-Allow-Headers", "Origin, No-Cache, X-Requested-With, If-Modified-Since, Pragma, Last-Modified, Cache-Control, Expires, Content-Type, X-E4M-With,userId,token");
+            response.setHeader("Access-Control-Allow-Credentials", "true");
+
+            if (request.getMethod().equals("OPTIONS")) {
+                response.setStatus(HttpServletResponse.SC_OK);
+            } else {
+                chain.doFilter(servletRequest, servletResponse);
+            }
+        }
+    }
+}
